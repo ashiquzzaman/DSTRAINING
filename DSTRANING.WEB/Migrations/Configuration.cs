170e96dba5302b1fe4e46eef9c4a96e10f527500
Ashiquzzaman <ashiquzzaman@outlook.com>
2019-01-28 11:07:00 +0600

    Add project files.

---
+++ DSTRANING.WEB/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+using System.Data.Entity.Migrations;
+using DSTRANING.WEB.Models;
+
+namespace DSTRANING.WEB.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ DSTRANING.WEB/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DSTRANING.WEB.Models
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
